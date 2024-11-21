86def7faf497cd265ca0cd971d51431b2f4945ca
code malayalam <nikithajoemon@gmail.com>
2024-11-21 21:03:37 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -8,26 +8,10 @@ import Button from './component/Button';
 
 function App() {
 
-const [clickedButton, setClickedButton] = useState(null)
-
-const handleClick =  (buttonId) => {
-   setClickedButton(buttonId);
-}
-
- 
-  return (
-    <div className="App">
-      <div className='app-container'>
-        <h1>TODO App</h1>
-          <div className='button-container'>
-             <Button color="#8888b9" label="Todo" onClick={() => {handleClick("button1")}}  isClicked={clickedButton === "button1"} isDisabled={false} />
-             <Button color="#8888b9" label="Todo list" onClick={() => {handleClick("button2")}}  isClicked={clickedButton === "button2"} isDisabled={false}/>
-          </div>
-      </div>
-    </div>
-  );
 }
 
 export default App;
+
+                 
     
      
--- src/component/Button.js
+++ src/component/Button.js
@@ -1,21 +1,3 @@
-import React from "react"
 
-const Button = ({ color, label, onClick, isClicked, isDisabled}) => {
-   
-
-    
-
-    return (
-        <button 
-            style={{ backgroundColor: isClicked ? "#3558b9": color }} 
-            onClick={onClick}
-            isDisabled={isDisabled}
-        >
-            {label}
-        </button>
-    )
-}
-
-export default Button;
  
  
