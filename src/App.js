d26781dffdcc8aafb7933b6938538c2109dbe7ae
nikitha2000 <60867872+nikitha2000@users.noreply.github.com>
2024-11-23 12:05:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,43 +4,33 @@ import Button from './component/Button';
 import ToDoForm from "./component/TodoForm";
 
 function App() {
-
-
-  const [clickedButton, setClickedButton] = useState(false)
-  const[todos, setTodo] = useState([])
+  const [clickedButton, setClickedButton] = useState(false);
+  const[todos, setTodo] = useState([]);
  
-
-  const handleClick =  (buttonId) => {
-     setClickedButton(buttonId);
-  }
-
- 
+const layoutToggleButtonClick =  (buttonID) => {
+    setClickedButton(buttonID);
+ };
+  
   const addTodo = (newTodo) => {
-    setTodo([...todos, newTodo])
-}
-
-
-   
-    return (
+    setTodo([...todos, newTodo]);
+};
+  
+ return (
       <div className="App">
         <div className='app-container'>
           <h1>TODO App</h1>
-            <div className='button-container'>
-               <Button color="#8888b9" label="Todo" onClick={() => {handleClick("button1")}}  isClicked={clickedButton === "button1"}  />
-               <Button color="#8888b9" label="Todo list" onClick={() => {handleClick("button2")}}  isClicked={clickedButton === "button2"} />
+            <div className='layout-button-container'>
+               <Button color="#8888b9" label="Todo" onClick={() => {layoutToggleButtonClick("todoButton")}}  isClicked={isTodoButtonClicked}  />
+               <Button color="#8888b9" label="Todo list" onClick={() => {layoutToggleButtonClick("todoListButton")}}  isClicked={isTodoListButtonClicked} />
             </div>
-           
-            {clickedButton=='button1' && (
+
+           {isTodoButtonClicked && (
               <ToDoForm onAddTodo={addTodo} />
             )}
-
-            
         </div>
       </div>
     );
-
-}
-
+};
 export default App;
 
 
