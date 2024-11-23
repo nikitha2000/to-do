720011b7cfca0c3323a7c2fd9c9f3db94d1954ae
code malayalam <nikithajoemon@gmail.com>
2024-11-23 11:24:37 +0530

    reviewed and corrected

--- src/App.js
+++ src/App.js
@@ -1,50 +1,39 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import './App.css';
 import Button from './component/Button';
-
+import ToDoForm from "./component/TodoForm";
 
 function App() {
-
-
-  const [clickedButton, setClickedButton] = useState(false)
-  const[todos, setTodo] = useState([])
+  const [clickedButton, setClickedButton] = useState(false);
+  const[todos, setTodos] = useState([]);
  
+  const layoutToggleButtonClick =  (buttonID) => {
+     setClickedButton(buttonID);
+  };
 
-  const handleClick =  (buttonId) => {
-     setClickedButton(buttonId);
-  }
-
- 
   const addTodo = (newTodo) => {
-    setTodo([...todos, newTodo])
-}
+    setTodos([...todos, newTodo]);
+};
 
+const isTodoButtonClicked = clickedButton == 'todoButton';
+const isTodoListButtonClicked = clickedButton == 'todoListButton';
 
-   
     return (
       <div className="App">
         <div className='app-container'>
           <h1>TODO App</h1>
-            <div className='button-container'>
-               <Button color="#8888b9" label="Todo" onClick={() => {handleClick("button1")}}  isClicked={clickedButton === "button1"}  />
-               <Button color="#8888b9" label="Todo list" onClick={() => {handleClick("button2")}}  isClicked={clickedButton === "button2"} />
+            <div className='layout-button-container'>
+               <Button color="#8888b9" label="Todo" onClick={() => {layoutToggleButtonClick("todoButton")}}  isClicked={isTodoButtonClicked}  />
+               <Button color="#8888b9" label="Todo list" onClick={() => {layoutToggleButtonClick("todoListButton")}}  isClicked={isTodoListButtonClicked} />
             </div>
            
-            {clickedButton=='button1' && (
+            {isTodoButtonClicked && (
               <ToDoForm onAddTodo={addTodo} />
             )}
 
-            
         </div>
       </div>
     );
+};
 
-}
-
-export default App;
-
-
-                 
-    
-
-     
+export default App;       
